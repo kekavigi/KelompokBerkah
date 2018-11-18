e2aea96d7bb362a468e896366fbfa1d7f41ac9f5
Dandi99 <d.agusmaulana99@gmail.com>
2018-11-18 16:16:44 +0700

    Add files via upload

---
+++ ADT/pesanan.c
@@ -0,0 +1,39 @@
+#include "pesanan.h"
+
+void AddElmtP(Pesanan * T, ElType X){
+	Elmt(*T, Neff(*T)++) = X;
+}
+
+IdxType SearchIdx (Pesanan T, ElType X){
+	IdxType i;
+	if (IsEmpty(T))
+		return IdxUndef;
+	else {
+		for (i=1; (i<=NbElmt(T)) && (Elmt(T,i)!=X); i++);
+		if (Elmt(T,i)==X) return i;
+	};
+}
+
+void DelElmtP(Pesanan * T, ElType X){
+	IdxType i;
+
+	if (SearchIdx(*T,X) != IdxUndef)
+		for (i=SearchIdxP(*T,X); i<NbElmt(*T); i++)
+			Elmt(T,i) = Elmt(T,i+1);
+	Neff(*T)--;
+}
+
+int NbElmtP (Pesanan T){
+	return Neff(T);
+}
+void delPesanan(Pesanan *DaftarPesanan,ElType pesanan){
+	//Menghapus pesanan dari daftar pesanan, pesanan pasti ada di daftar pesanan
+		DelElmtP(DaftarPesanan,&pesanan);
+	}
+		
+}
+
+void addPesanan(ElType pesanan,Pesanan *DaftarPesanan){
+	//Menambah pesanan ke daftar pesanan
+	AddElmtP(DaftarPesanan,pesanan);
+}
