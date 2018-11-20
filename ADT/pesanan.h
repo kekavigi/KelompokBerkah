2c35c52eeb69b8d5edc2d6a136245eaa077dc748
Dandi99 <d.agusmaulana99@gmail.com>
2018-11-20 12:23:16 +0700

    Update pesanan.h

--- ADT/pesanan.h
+++ ADT/pesanan.h
@@ -5,7 +5,7 @@
 #define IdxUndef -999 
 /* Indeks tak terdefinisi*/
 
-typedef char ElType; /* type indeks */
+typedef int ElType; /* type indeks */
 typedef int IdxType;  /* type elemen tabel */
 
 typedef struct { 
@@ -18,6 +18,7 @@ typedef struct {
 #define Number(T)   (T).Number
 #define TI(T)     (T).TI
 #define Elmt(T,i) (T).TI[(i)]
+
 int NbElmtP (Pesanan T);
 
 IdxType SearchIdxP (Pesanan T, ElType X);
