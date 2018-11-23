e12273c8ff42b9bc08a44b7e68bb126f0c235629
Dandi99 <d.agusmaulana99@gmail.com>
2018-11-24 01:37:36 +0700

    Add files via upload

---
+++ ADT/komplitstack.h
@@ -0,0 +1,12 @@
+#include "stackt.h"
+#include "array.h"
+#include "bintree.h"
+
+#ifndef KOMPLITSTACK_H
+#define KOMPLITSTACK_H
+
+void CH(Stack *Hand);
+void CT(Stack *Nampan);
+void TAKE(Stack *Hand,infotypeStackt bahan);
+void ShowRecipe (BinTree P);
+void PUT (BinTree  P, Stack *Bahan,Stack *Food);
