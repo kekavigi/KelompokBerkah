4e26bc42bde1a7999ebf042cc29425c6dbaa8c03
Dandi99 <d.agusmaulana99@gmail.com>
2018-11-24 10:32:22 +0700

    Update komplitstack.c

--- ADT/komplitstack.c
+++ ADT/komplitstack.c
@@ -5,6 +5,7 @@
 Stack Hand;
 Stack Nampan;
 TabInt Pesanan;
+
 void CH(Stack *Hand){
 	//Membuang semua makanan di hand
 	infotypeStackt bahan;
@@ -12,6 +13,7 @@ void CH(Stack *Hand){
 		Pop(Hand,&bahan);
 	}
 }
+
 void CT(Stack *Nampan){
 	//Membuang semua makanan di tray
 	infotypeStackt makanan;
@@ -28,10 +30,8 @@ void TAKE(Stack *Hand,infotypeStackt bahan){
 void ShowRecipe (BinTree P)
 /* Mencetak Resep */
 {
-  //Algoritma
   ShowBranch(P);
 }
-
 
 void PUT (BinTree  P, Stack *Bahan,Stack *Food)
 {
@@ -44,26 +44,25 @@ void PUT (BinTree  P, Stack *Bahan,Stack *Food)
   CreateEmptyStackt(&FoodExtra);
   CreateEmptyStackt(&SIn);
   while(!IsEmptyStackt(*Bahan)){
-    while ((InfoTop(*Bahan) != 1) && (!IsEmptyStackt(*Bahan))){ //Cari piring di Stack
+    while ((InfoTop(*Bahan) != 1) && (!IsEmptyStackt(*Bahan))){ 
       Pop(Bahan,&X);
       Push(&SIn,X);
     }
-    if (InfoTop(*Bahan) == 1){ //Kalo udah ketemu piring di Top Bahan
+    if (InfoTop(*Bahan) == 1){ 
       CreateEmptyStackt(&SOut);
       //taro piring di SIn
       Pop(Bahan,&X);
       Push(&SIn,X);
       Px = P; 
       SearchBranch(&Px, &SIn, &SOut); 
-      if (IsOneElmt(Px)) { //Kalo sesuai resep
-        Push(&FoodExtra, Akar(Px)); //biar dibalik dulu stacknya
-        //Jika ada makanan yang sisa di SIn, balikin ke SLoop dulu
+      if (IsOneElmt(Px)) { 
+        Push(&FoodExtra, Akar(Px)); 
         while (!IsEmptyStackt(SIn)){
           Pop(&SIn,&X);
           Push(&SLoop,X);
         }
       }
-      else { //gak sesuai resep, balikin ke SLoop dulu
+      else { 
         CreateEmptyStackt(&SExtra);
         while(!(IsEmptyStackt(SOut))){
           Pop(&SOut,&X);
@@ -79,16 +78,15 @@ void PUT (BinTree  P, Stack *Bahan,Stack *Food)
         }
       }
     }
-    else { //Gak ada piring di stack Bahan 
-      //Balikin SIn ke SLoop dulu
+    else { 
+ 
       while (!IsEmptyStackt(SIn)){
         Pop(&SIn,&X);
         Push(&SLoop,X);
       }
     }
   }
-  //kalo SLoop gak kosong, balikin semuanya ke Bahan
-  //CreateEmptyStackt(&SIn);
+
   while(!IsEmptyStackt(SLoop)){
     Pop(&SLoop,&X);
     Push(Bahan,X);
@@ -98,4 +96,3 @@ void PUT (BinTree  P, Stack *Bahan,Stack *Food)
     Push(Food,X);
   } 
 }
-//int CekRacikan(Stack *Hand);
